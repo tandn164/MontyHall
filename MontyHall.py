c94ce4426d0320499bde4402bff3aa397dbf3a4a
tan.nguyen <tan.nguyen2@ekotek.vn>
2024-09-26 15:49:35 +0700

    beautify

--- MontyHall.py
+++ MontyHall.py
@@ -9,27 +9,27 @@ def create_random_doors():
     
     return doors
 
+def check_door(doors, selected_door):
+    if doors[selected_door] == 'car':
+        return 1
+    return 0
+
 def select_door(doors, door_number, option):
     available_doors = [key for key in doors if key != door_number]
+
     if len(doors) > 2:
         # MC select
         goat_doors = [key for key in available_doors if doors[key] != "car"]
         random_door = random.choice(goat_doors)
         new_doors = {key: value for key, value in doors.items() if key != random_door}
         return select_door(new_doors, door_number, option)
-    else:
-        if (option == 1):
-            # User change
-            changed_door = random.choice(available_doors)
-            if doors[changed_door] == 'car':
-                return 1
-            return 0
-        if (option == 2):
-            # Assumpt that we play again
-            new_door = random.choice([key for key in doors])
-            if doors[new_door] == 'car':
-                return 1
-            return 0
+    
+    if (option == 1):
+        # User change
+        return check_door(doors, random.choice(available_doors))
+    if (option == 2):
+        # Assumpt that we play again
+        return check_door(doors, random.choice([key for key in doors]))
 
 win = 0
 for _ in range(try_count):
